75692dca5a58b934f0a048ccfb49261a4733406b
Cheng Li <cli99@illinois.edu>
2017-11-20 14:50:17 -0600

    test pinned memcpy

--- src/cudamemcpy.cpp
+++ src/cudamemcpy.cpp
@@ -28,3 +28,34 @@ static void CUDAMemcpyToGPU(benchmark::State &state) {
   delete[] src;
 }
 BENCHMARK(CUDAMemcpyToGPU)->DenseRange(1, 32, 1);
+
+static void CUDAPinnedMemcpyToGPU(benchmark::State &state) {
+  const auto bytes = 1ULL << static_cast<size_t>(state.range(0));
+  // char *src = new char[bytes];
+  char *src;
+  auto err = cudaMallocHost(&src, bytes);
+  if (err != cudaSuccess) {
+    state.SkipWithError("failed to perform cudaMallocHost");
+    return;
+  }
+  char *dst = nullptr;
+  err = cudaMalloc(&dst, bytes);
+  if (err != cudaSuccess) {
+    state.SkipWithError("failed to perform cudaMalloc");
+    return;
+  }
+  for (auto _ : state) {
+    const auto err = cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+      state.SkipWithError("failed to perform cudaMemcpy");
+      break;
+    }
+  }
+  state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
+  state.counters.insert({{"bytes", bytes}});
+  if (dst) {
+    cudaFree(dst);
+  }
+  delete[] src;
+}
+BENCHMARK(CUDAPinnedMemcpyToGPU)->DenseRange(1, 32, 1);
