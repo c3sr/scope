5b092c1ee2e146b865b135af6d4c5c3d249aa01a
Abdul Dakkak <abduld@wolfram.com>
2017-12-27 05:21:26 +0000

    format

--- src/daxpy.cpp
+++ src/daxpy.cpp
@@ -13,16 +13,17 @@ static void DAXPY(benchmark::State &state) {
   const auto y_incr = state.range(2);
   const auto alpha = 0.5;
 
-    auto x = std::vector<double>(N);
-    auto y = std::vector<double>(N);
-    std::iota(x.begin(), x.end(), 1);
-    std::iota(y.begin(), y.end(), 1);
+  auto x = std::vector<double>(N);
+  auto y = std::vector<double>(N);
+  std::iota(x.begin(), x.end(), 1);
+  std::iota(y.begin(), y.end(), 1);
   for (auto _ : state) {
-    cblas_daxpy(N, alpha, x.data(), x_incr, y.data(), y_incr); 
+    cblas_daxpy(N, alpha, x.data(), x_incr, y.data(), y_incr);
   }
- 
-  state.counters.insert({{"N", N}, {"x_increment", x_incr}, {"y_increment", y_incr}});
-  state.SetBytesProcessed(int64_t(state.iterations()) * 3*N);
+
+  state.counters.insert(
+      {{"N", N}, {"x_increment", x_incr}, {"y_increment", y_incr}});
+  state.SetBytesProcessed(int64_t(state.iterations()) * 3 * N);
 }
 
 BENCHMARK(DAXPY) // N, DA, INCX, INCY
