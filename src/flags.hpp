a55dceb5ae2e2fb24da9a5d861dd8901ffb1368e
Abdul Dakkak <abduld@wolfram.com>
2017-12-28 18:25:29 +0000

    ...

--- src/flags.hpp
+++ src/flags.hpp
@@ -1,20 +1,33 @@
 #pragma once
 
 #include <cxxopts.hpp>
+#include <iostream>
 
 #include "config.hpp"
 
 static int cuda_device_id = 0;
 
 static void init_flags(int argc, char **argv) {
-  cxxopts::Options options(argv[0], "microbenchmark suite");
+  try {
+    cxxopts::Options options(argv[0], "microbenchmark suite");
 
-  options.add_options()("d,debug", "Enable debugging")(
-      "cuda_device", "CUDA device to use",
-      cxxopts::value<int>()->default_value("0"))("f,file", "File name",
-                                                 cxxopts::value<std::string>());
+    options.positional_help("[optional args]").show_positional_help();
+    options.add_options()("d,debug", "Enable debugging")(
+        "cuda_device", "CUDA device to use",
+        cxxopts::value<int>()->default_value("0"))(
+        "f,file", "File name", cxxopts::value<std::string>());
 
-  auto result = options.parse(argc, argv);
+    auto result = options.parse(argc, argv);
 
-  cuda_device_id = result["cuda_device"].as<int>();
+    if (result.count("help")) {
+      std::cout << options.help({}) << std::endl;
+      exit(0);
+    }
+
+    cuda_device_id = result["cuda_device"].as<int>();
+  } catch (const cxxopts::OptionException &e) {
+    // std::cout << "error parsing options: " << e.what() << std::endl;
+  }
+
+  return;
 }
