d4b0e0ee3be04da745cdd4390917a4cd540f356e
Cheng Li <cli99@illinois.edu>
2018-01-23 01:00:49 +0800

    use benchmark for lock

---
+++ src/lock/args.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#define ARGS_FULL()                                                                                                    \
+  Args({1})
+      ->Args({10})                                                                                                  \
+      ->Args({100})                                                                                              \
+      ->Args({1000})                                                                                             \
+      ->Args({10000})                                                                                            \
+      ->Args({100000})                                                                                           \
+      ->Args({10000000})                                                                                         \
+      ->Args({100000000})                                                                                        \
+      ->Args({1000000000})                                                                                       \
+      ->ArgNames({"N"})
