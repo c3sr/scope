cdd7fd22c3a84c6be6a55bc481ab0832f55510a8
Cheng Li <cli99@illinois.edu>
2018-01-23 01:10:19 +0800

    use benchmark for lock

--- src/utils/sugar.cmake
+++ src/utils/sugar.cmake
@@ -20,6 +20,7 @@ sugar_files(
     mpl.hpp
     transwarp.h
     hostname.hpp
+    b5_papi.hpp
     cuda.hpp
     error.hpp
     nvptx.hpp
@@ -39,6 +40,8 @@ sugar_files(
 
 sugar_files(
     BENCHMARK_SOURCES
+    papi.cpp
     commandlineflags.cpp
+    b5_papi.cpp
 )
 
