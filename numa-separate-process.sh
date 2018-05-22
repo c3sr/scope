e1ae85f10ff43686947d43194af13764e98f8c12
Carl Pearson <pearson@illinois.edu>
2018-05-22 15:24:19 -0500

    add argument for benchmark variable

--- numa-separate-process.sh
+++ numa-separate-process.sh
@@ -4,58 +4,34 @@
 
 set -eou pipefail -x
 
+# Which machine to use (s822lc, ac922, or dgx)
 machine=$1
+# Path to bench executable
+BENCH=$2
 
-s822lc_gpus=(
-0
-1
-2
-3
-)
+s822lc_gpus=( 0 1 2 3 )
+s822lc_numas=( 0 1 )
+ 
+ac922_gpus=( 0 1 2 3 )
+ac922_numas=( 0 8 )
 
-s822lc_numas=(
-0
-1
-)
-
-ac922_gpus=(
-0
-1
-2
-3
-)
-
-ac922_numas=(
-0
-8
-)
-
-dgx_gpus=(
-0
-1
-2
-3
-4
-5
-6
-7
-)
-
-dgx_numas=(
-0
-1
-)
+dgx_gpus=( 0 1 2 3 4 5 6 7 )
+dgx_numas=( 0 1 )
 
 numas=$machine\_numas[@]
 gpus=$machine\_gpus[@]
 
+
 numa_gpu_gpu_bmarks=(
 NUMA_Memcpy_GPUToGPU
+noop
 )
 
 gpu_gpu_bmarks=(
+CUDA_Memcpy_GPUToGPU
 UM_Coherence_GPUToGPU
 UM_Prefetch_GPUToGPU
+noop
 )
 
 numa_gpu_bmarks=(
@@ -67,36 +43,44 @@ NUMAUM_Coherence_GPUToHost
 NUMAUM_Coherence_HostToGPU
 NUMAUM_Prefetch_GPUToHost
 NUMAUM_Prefetch_HostToGPU
+noop
 )
 
-#for b in "${numa_gpu_bmarks[@]}"; do
-#    for n in ${!numas}; do
-#        for g in ${!gpus}; do
-#            ./bench --benchmark_filter="$b.*/$n/$g/" --benchmark_out=`hostname`-$b-$n-$g.json --benchmark_repetitions=5;
-#        done
-#    done
-#done
-#
-#for b in "${gpu_gpu_bmarks[@]}"; do
-#    for g1 in ${!gpus}; do
-#        for g2 in ${!gpus}; do
-#                if [ "$g2" != "$g1" ]; then
-#                    ./bench --benchmark_filter="$b.*/$g1/$g2/" --benchmark_out=`hostname`-$b-$g1-$g2.json --benchmark_repetitions=5;
-#                fi
-#        done
-#    done
-#done
+
+for b in "${numa_gpu_bmarks[@]}"; do
+    if [ "$b" != "noop" ]; then
+        for n in ${!numas}; do
+            for g in ${!gpus}; do
+                "$BENCH" --benchmark_filter="$b.*/$n/$g/" --benchmark_out=`hostname`-$b-$n-$g.json --benchmark_repetitions=5;
+            done
+        done
+    fi
+done
+
+for b in "${gpu_gpu_bmarks[@]}"; do
+    if [ "$b" != "noop" ]; then
+        for g1 in ${!gpus}; do
+            for g2 in ${!gpus}; do
+                    if [ "$g2" != "$g1" ]; then
+                        "$BENCH" --benchmark_filter="$b.*/$g1/$g2/" --benchmark_out=`hostname`-$b-$g1-$g2.json --benchmark_repetitions=5;
+                    fi
+            done
+        done
+    fi
+done
 
 
 
 for b in "${numa_gpu_gpu_bmarks[@]}"; do
-    for n in ${!numas}; do
-        for g1 in ${!gpus}; do
-            for g2 in ${!gpus}; do
-                if [ "$g2" != "$g1" ]; then
-                    ./bench --benchmark_filter="$b.*/$n/$g1/$g2/" --benchmark_out=`hostname`-$b-$n-$g1-$g2.json --benchmark_repetitions=5;
-                fi
+    if [ "$b" != "noop" ]; then
+        for n in ${!numas}; do
+            for g1 in ${!gpus}; do
+                for g2 in ${!gpus}; do
+                    if [ "$g2" != "$g1" ]; then
+                        "$BENCH" --benchmark_filter="$b.*/$n/$g1/$g2/" --benchmark_out=`hostname`-$b-$n-$g1-$g2.json --benchmark_repetitions=5;
+                    fi
+                done
             done
         done
-    done
+    fi
 done
