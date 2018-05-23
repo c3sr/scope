a1d81f345b469f62010cef09f81807cbf6bad836
Carl Pearson <pearson@illinois.edu>
2018-05-23 17:32:01 -0500

    set strict bind policy

--- src/init/numa.cpp
+++ src/init/numa.cpp
@@ -23,9 +23,14 @@ bool init_numa() {
   }
 
   numa_set_strict(1);
+  LOG(debug, "set numa_set_strict(1)");
+  numa_set_bind_policy(1);
+  LOG(debug, "set numa_set_bind_policy(1)");
 
   numa_exit_on_warn = 1;
+  LOG(debug, "set numa_exit_on_warm = 1");
   numa_exit_on_error = 1;
+  LOG(debug, "set numa_exit_on_error = 1");
 
   return false;
 }
