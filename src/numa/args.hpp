e4047865c9545e56d94ad7c2e6a6386b56c9dfb8
Carl Pearson <pearson@illinois.edu>
2018-05-27 15:25:44 -0500

    Update separate processes script

--- src/numa/args.hpp
+++ src/numa/args.hpp
@@ -1,7 +1,7 @@
 inline
 static void ArgsCountNumaNuma(benchmark::internal::Benchmark* b) {
 
-  for (int j = 16; j <= 32; ++j) { // log2(bytes)
+  for (int j = 12; j <= 32; ++j) { // log2(bytes)
       for (auto src_numa : numa_nodes()) {
         for (auto dst_numa : numa_nodes()) {
             b->Args({j, src_numa, dst_numa});
