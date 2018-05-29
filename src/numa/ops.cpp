189152a9a0461588ebbeb572ac24703df1beb095
Carl Pearson <pearson@illinois.edu>
2018-05-29 14:50:25 -0500

    use OpenMP for numa_to_numa

--- src/numa/ops.cpp
+++ src/numa/ops.cpp
@@ -11,11 +11,12 @@ void rd_8(char *ptr, const size_t count, const size_t stride)
     const size_t elemsPerStride = stride / sizeof(int64_t);
 
     int64_t acc = 0;
+    #pragma omp parallel for schedule(static) private(acc)
     for (size_t i = 0; i < numElems; i += elemsPerStride)
     {
         benchmark::DoNotOptimize(acc += dp[i]);
-    }
     benchmark::ClobberMemory();
+    }
 }
 
 void wr_8(char *ptr, const size_t count, const size_t stride)
@@ -24,10 +25,10 @@ void wr_8(char *ptr, const size_t count, const size_t stride)
 
     const size_t numElems = count / sizeof(int64_t);
     const size_t elemsPerStride = stride / sizeof(int64_t);
-
+    #pragma omp parallel for schedule(static)
     for (size_t i = 0; i < numElems; i += elemsPerStride)
     {
         benchmark::DoNotOptimize(dp[i] = i);
-    }
     benchmark::ClobberMemory();
+    }
 }
--- src/numa/rd.cpp
+++ src/numa/rd.cpp
@@ -23,13 +23,14 @@ static void NUMA_RD(benchmark::State &state) {
     }
 
 
-    const auto bytes = (1ULL << static_cast<size_t>(state.range(0))) / state.threads;
-    const int src_numa = state.range(1);
-    const int dst_numa = state.range(2);
+    const size_t threads = state.range(0);
+    const auto bytes = 1ULL << static_cast<size_t>(state.range(1));
+    const int src_numa = state.range(2);
+    const int dst_numa = state.range(3);
 
   // Setup
     const long pageSize = sysconf(_SC_PAGESIZE);
-    numa_bind_node(src_numa);
+    omp_numa_bind_node(src_numa);
     char *ptr = static_cast<char *>(aligned_alloc(pageSize, bytes));
 
     std::memset(ptr, 0, bytes);
@@ -38,7 +39,7 @@ static void NUMA_RD(benchmark::State &state) {
   for (auto _ : state) {
     state.PauseTiming();
     // invalidate data in dst cache
-    numa_bind_node(src_numa);
+    omp_numa_bind_node(src_numa);
     //for (size_t i = 0; i < bytes; ++i) {
     //   ptr[i] = rand();
     //}
@@ -47,14 +48,14 @@ static void NUMA_RD(benchmark::State &state) {
     benchmark::ClobberMemory();
 
     // Access from Device and Time
-    numa_bind_node(dst_numa);
+    omp_numa_bind_node(dst_numa);
     state.ResumeTiming();
 
     rd_8(ptr, bytes, 8);
 
   }
 
-  numa_bind_node(-1);
+  omp_numa_bind_node(-1);
 
     state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
     state.counters.insert({{"bytes", bytes}});
@@ -64,4 +65,4 @@ static void NUMA_RD(benchmark::State &state) {
 }
 
 
-BENCHMARK(NUMA_RD)->ThreadRange(1,8)->Apply(ArgsCountNumaNuma)->MinTime(0.1)->UseRealTime();
+BENCHMARK(NUMA_RD)->Apply(ArgsThreadCountNumaNuma)->MinTime(0.1)->UseRealTime();
