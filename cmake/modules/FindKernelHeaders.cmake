9a41f33eedd18b4066a911c22c994b0924679c07
Carl Pearson <pearson@illinois.edu>
2018-05-31 17:13:24 -0500

    Add FindKernelHeaders cmake for the future

---
+++ cmake/modules/FindKernelHeaders.cmake
@@ -0,0 +1,29 @@
+# Find the kernel release
+execute_process(
+  COMMAND uname -r
+  OUTPUT_VARIABLE KERNEL_RELEASE
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Find the headers
+find_path(KERNELHEADERS_DIR
+  include/linux/user.h
+  PATHS /usr/src/linux-headers-${KERNEL_RELEASE}
+)
+
+message(STATUS "Kernel release: ${KERNEL_RELEASE}")
+message(STATUS "Kernel headers: ${KERNELHEADERS_DIR}")
+
+if (KERNELHEADERS_DIR)
+  set(KERNELHEADERS_INCLUDE_DIRS
+    ${KERNELHEADERS_DIR}/include
+    CACHE PATH "Kernel headers include dirs"
+  )
+  set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
+else (KERNELHEADERS_DIR)
+  set(KERNELHEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
+endif (KERNELHEADERS_DIR)
+
+message(STATUS "Kernel headers includes: ${KERNELHEADERS_INCLUDE_DIRS}")
+
+mark_as_advanced(KERNELHEADERS_FOUND)
