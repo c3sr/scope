5f1448e0e2c725e9b727725f28bd3bd518d8ce09
Carl Pearson <pearson@illinois.edu>
2018-06-06 14:29:46 -0700

    Clean up example

--- src/example/example.cpp
+++ src/example/example.cpp
@@ -3,79 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <omp.h>
-#include <numa.h>
-
 #include "init/init.hpp"
 #include "utils/utils.hpp"
-/*
-#define NAME "NUMA/WR"
-
-static void NUMA_WR(benchmark::State &state) {
-
-    if (!has_numa) {
-        state.SkipWithError(NAME " NUMA not available");
-        return;
-    }
-
-    const int threads = state.range(0);
-    const auto bytes = 1ULL << static_cast<size_t>(state.range(1));
-    const int src_numa = state.range(2);
-    const int dst_numa = state.range(3);
-
-    omp_set_num_threads(threads);
-    if (threads != omp_get_max_threads()) {
-      state.SkipWithError(NAME " unable to set OpenMP threads");
-      return;
-    }
-
-  // Setup
-    const long pageSize = sysconf(_SC_PAGESIZE);
-    omp_numa_bind_node(dst_numa);
-    char *ptr = static_cast<char *>(aligned_alloc(pageSize, bytes));
-    std::memset(ptr, 0, bytes);
-
-  for (auto _ : state) {
-    state.PauseTiming();
-
-    omp_numa_bind_node(dst_numa);
-
-    std::memset(ptr, 0, bytes);
-    benchmark::DoNotOptimize(ptr);
-    benchmark::ClobberMemory();
-
-    omp_numa_bind_node(src_numa);
-    state.ResumeTiming();
-
-    wr_8(ptr, bytes, 8);
-  }
-
-  omp_numa_bind_node(-1);
-
-  state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
-  state.counters.insert({{"bytes", bytes}});
-
-  free(ptr);
-
-}
 
 
-BENCHMARK(NUMA_WR)->Apply(ArgsThreadCountNumaNuma)->MinTime(0.1)->UseRealTime();
-*/
-
-static void BM_StringCreation(benchmark::State& state) {
+static void EXAMPLE1(benchmark::State& state) {
   for (auto _ : state)
     std::string empty_string;
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(EXAMPLE1);
 
 // Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
+static void EXAMPLE2(benchmark::State& state) {
   std::string x = "hello";
   for (auto _ : state)
     std::string copy(x);
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(EXAMPLE2);
 
-//BENCHMARK_MAIN();
+
