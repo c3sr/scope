08313e8028d331d0712bb05b3b1b7e52944ec335
Sarah Hashash <hashash2@.illinois.edu>
2018-06-05 21:46:18 -0500

    Added BM_StringCreation example

--- src/example/example.cpp
+++ src/example/example.cpp
@@ -8,7 +8,7 @@
 
 #include "init/init.hpp"
 #include "utils/utils.hpp"
-
+/*
 #define NAME "NUMA/WR"
 
 static void NUMA_WR(benchmark::State &state) {
@@ -61,3 +61,21 @@ static void NUMA_WR(benchmark::State &state) {
 
 
 BENCHMARK(NUMA_WR)->Apply(ArgsThreadCountNumaNuma)->MinTime(0.1)->UseRealTime();
+*/
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+//BENCHMARK_MAIN();
