ab1cb48473ffce9043ec845f655bb3e8dd86ceb8
Carl Pearson <pearson@illinois.edu>
2018-06-07 06:22:05 -0700

    Remove USE_CUDA_EVENTS from memcpy/host_to_gpu.cpp

--- src/memcpy/host_to_gpu.cpp
+++ src/memcpy/host_to_gpu.cpp
@@ -29,23 +29,17 @@ static void CUDA_Memcpy_HostToGPU(benchmark::State &state) {
   }
   defer(cudaFree(dst));
 
-#ifdef USE_CUDA_EVENTS
   cudaEvent_t start, stop;
   PRINT_IF_ERROR(cudaEventCreate(&start));
   PRINT_IF_ERROR(cudaEventCreate(&stop));
-#endif // USE_CUDA_EVENTS
 
   for (auto _ : state) {
-#ifdef USE_CUDA_EVENTS
     cudaEventRecord(start, NULL);
-#endif // USE_CUDA_EVENTS
 
     const auto cuda_err = cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice);
 
-#ifdef USE_CUDA_EVENTS
     cudaEventRecord(stop, NULL);
     cudaEventSynchronize(stop);
-#endif // USE_CUDA_EVENTS
 
     state.PauseTiming();
 
@@ -53,22 +47,17 @@ static void CUDA_Memcpy_HostToGPU(benchmark::State &state) {
       state.SkipWithError("CUDA/MEMCPY/HostToGPU failed to perform memcpy");
       break;
     }
-#ifdef USE_CUDA_EVENTS
     float msecTotal = 0.0f;
     if (PRINT_IF_ERROR(cudaEventElapsedTime(&msecTotal, start, stop))) {
       state.SkipWithError("CUDA/MEMCPY/HostToGPU failed to get elapsed time");
       break;
     }
     state.SetIterationTime(msecTotal / 1000);
-#endif // USE_CUDA_EVENTS
     state.ResumeTiming();
   }
   state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(bytes));
   state.counters.insert({{"bytes", bytes}});
 }
 
-#ifdef USE_CUDA_EVENTS
+
 BENCHMARK(CUDA_Memcpy_HostToGPU)->ALL_ARGS()->UseManualTime();
-#else  // USE_CUDA_EVENTS
-BENCHMARK(CUDA_Memcpy_HostToGPU)->ALL_ARGS();
-#endif // USE_CUDA_EVENTS
