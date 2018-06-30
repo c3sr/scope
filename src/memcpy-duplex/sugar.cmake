81e6647d666c1516c3e8b229a90fdcf08bf09139
Carl Pearson <pearson@illinois.edu>
2018-06-30 07:09:03 -0400

    Fix broken merge in src/memcpy-duplex/sugar.cmake

--- src/memcpy-duplex/sugar.cmake
+++ src/memcpy-duplex/sugar.cmake
@@ -3,17 +3,10 @@
 # see wiki for more info:
 #   https://github.com/ruslo/sugar/wiki/Collecting-sources
 
-<<<<<<< HEAD
-if(DEFINED MEMCPY_DUPLEX_SUGAR_CMAKE_)
-  return()
-else()
-  set(MEMCPY_DUPLEX_SUGAR_CMAKE_ 1)
-=======
 if(DEFINED SRC_MEMCPY_DUPLEX_SUGAR_CMAKE_)
   return()
 else()
   set(SRC_MEMCPY_DUPLEX_SUGAR_CMAKE_ 1)
->>>>>>> 30886528961fead955a1713a60428ee8bb740ff0
 endif()
 
 include(sugar_files)
