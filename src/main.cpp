16ced017e8ff9af0ebbc5306e4dd847d6f31bc77
Carl Pearson <pearson@illinois.edu>
2018-08-21 15:38:43 -0500

    update comm_scope and example_scope

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,30 @@
 
 static const auto help = R"(
 scope [--benchmark_list_tests={true|false}]
-          [--benchmark_filter=<regex>]
-          [--benchmark_min_time=<min_time>]
-          [--benchmark_repetitions=<num_repetitions>]
-          [--benchmark_format=<tabular|json|csv>]
-          [--color_print={true|false}]
-          [--v=<verbosity>]
-          [ --version]
+      [--benchmark_filter=<regex>]
+      [--benchmark_min_time=<min_time>]
+      [--benchmark_repetitions=<num_repetitions>]
+      [--benchmark_format=<tabular|json|csv>]
+      [--color_print={true|false}]
+      [--v=<verbosity>]
+      [ --version]
 )";
-
-// const char *bench_git_refspec();
-// const char *bench_git_hash();
-// const char *bench_git_tag();
-
-// std::string bench_get_build_version() {
-//   return fmt::format("{} {} {}", bench_git_refspec(), bench_git_tag(), bench_git_hash());
-// }
 
 int main(int argc, char **argv) {
   init(argc, argv);
-
   // if (FLAG(help)) {
   //   std::cout << help << "\n";
   //   return 0;
   // }
 
-
-
-  benchmark::Initialize(&argc, argv);
-
   if (FLAG(version)) {
     std::cout << "Scope " << version() << "\n";
     return 0;
   }
+
+
+
+  benchmark::Initialize(&argc, argv);
 
   if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
 
