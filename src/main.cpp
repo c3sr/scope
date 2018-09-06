7db6ec6374192845561de29d6737760883fc06bb
Carl Pearson <carl.pearson@ibm.com>
2018-09-06 14:52:30 -0400

    don't report unrecognized flags in google benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv) {
 
   benchmark::Initialize(&argc, argv);
 
-  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  // if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
 
   // auto options = benchmark::internal::GetOutputOptions();
 
