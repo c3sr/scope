2cafd3456f07e3aa33e23fa0f929a978b77f5ce7
Carl Pearson <carl.pearson@us.ibm.com>
2018-09-27 13:01:57 -0400

    fix typos in ispass19 scripts

--- tools/generate_ispass19.sh
+++ tools/generate_ispass19.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
+set -xeuo pipefail
+
 # h2d and d2h
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_HostToGPU/0   > run_Comm_Duplex_NUMAMemcpy_HostToGPU_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_GPUToHost/0   > run_Comm_Duplex_NUMAMemcpy_GPUToHost_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_PinnedToGPU/0 > run_Comm_Duplex_NUMAMemcpy_PinnedToGPU_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_GPUToPinned/0 > run_Comm_Duplex_NUMAMemcpy_GPUToPinned_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_HostToGPU/0   > run_Comm_NUMAMemcpy_HostToGPU_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_GPUToHost/0   > run_Comm_NUMAMemcpy_GPUToHost_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_PinnedToGPU/0 > run_Comm_NUMAMemcpy_PinnedToGPU_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_GPUToPinned/0 > run_Comm_NUMAMemcpy_GPUToPinned_0.sh
 
 # h2d and d2h flush
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_HostToGPU_flush/0   > run_Comm_Duplex_NUMAMemcpy_HostToGPU_flush_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_GPUToHost_flush/0   > run_Comm_Duplex_NUMAMemcpy_GPUToHost_flush_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_PinnedToGPU_flush/0 > run_Comm_Duplex_NUMAMemcpy_PinnedToGPU_flush_0.sh
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_GPUToPinned_flush/0 > run_Comm_Duplex_NUMAMemcpy_GPUToPinned_flush_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_HostToGPU_flush/0   > run_Comm_NUMAMemcpy_HostToGPU_flush_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_GPUToHost_flush/0   > run_Comm_NUMAMemcpy_GPUToHost_flush_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_PinnedToGPU_flush/0 > run_Comm_NUMAMemcpy_PinnedToGPU_flush_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_NUMAMemcpy_GPUToPinned_flush/0 > run_Comm_NUMAMemcpy_GPUToPinned_flush_0.sh
+
+# h2d duplex
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_Host/0   > run_Comm_Duplex_NUMAMemcpy_Host_0.sh
+./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_Pinned/0 > run_Comm_Duplex_NUMAMemcpy_Pinned_0.sh
 
 # coherence
 ./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_UM_Coherence_HostToGPU/0 > run_Comm_UM_Coherence_HostToGPU_0.sh
@@ -28,8 +34,3 @@
 # d2d peer
 ./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Memcpy_GPUToGPUPeer > run_Comm_Memcpy_GPUToGPUPeer.sh
 
-# h2d pageable duplex
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_Host/0 > run_Comm_Duplex_NUMAMemcpy_Host_0.sh
-
-# h2d pinned duplex
-./generate_runscripts.py --benchmark-repetitions=5 --benchmark-filter=Comm_Duplex_NUMAMemcpy_Pinned/0 > run_Comm_Duplex_NUMAMemcpy_Pinned_0.sh
