802a7844813d91c1c48470aecfb647372501d186
Carl Pearson <carl.w.pearson@gmail.com>
2019-04-04 08:41:18 -0500

    remove extra semicolons

--- src/scope/init/flags.hpp
+++ src/scope/init/flags.hpp
@@ -4,10 +4,10 @@
 #include "scope/utils/commandlineflags.hpp"
 #include "clara/clara.hpp"
 
-DECLARE_FLAG_vec_int32(cuda_device_ids);
-DECLARE_FLAG_bool(help);
-DECLARE_FLAG_int32(verbose);
-DECLARE_FLAG_bool(version);
+DECLARE_FLAG_vec_int32(cuda_device_ids)
+DECLARE_FLAG_bool(help)
+DECLARE_FLAG_int32(verbose)
+DECLARE_FLAG_bool(version)
 
 
 
