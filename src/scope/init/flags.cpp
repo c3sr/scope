785ade2749f8072696d9da67bca320f18a0f9196
Carl Pearson <carl.w.pearson@gmail.com>
2019-04-04 08:46:28 -0500

    remove extra ;

--- src/scope/init/flags.cpp
+++ src/scope/init/flags.cpp
@@ -6,10 +6,10 @@
 
 
 
-DEFINE_FLAG_vec_int32(cuda_device_ids, {}, "The cuda devices to use");
-DEFINE_FLAG_bool(help, false, "Show help message.");
-DEFINE_FLAG_int32(verbose, 1, "Verbose level.");
-DEFINE_FLAG_bool(version, false, "Show version message.");
+DEFINE_FLAG_vec_int32(cuda_device_ids, {}, "The cuda devices to use")
+DEFINE_FLAG_bool(help, false, "Show help message.")
+DEFINE_FLAG_int32(verbose, 1, "Verbose level.")
+DEFINE_FLAG_bool(version, false, "Show version message.")
 
 void register_flags() {
     RegisterOpt(clara::Help(FLAG(help)));
